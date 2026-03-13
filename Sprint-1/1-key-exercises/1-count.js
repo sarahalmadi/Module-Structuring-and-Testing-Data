e08c39260b1146e908ab1ba992dfa882c2235e41
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-13 20:20:23 +0100

    Fix spell checker

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-In string 3 we are assigning the new value "1" to the variable "count". "=" is the assignment's operator, which assigns the value on the right (count + 1) to the variable on the left (count). So after line 3 executes, the variable "count" will have the new value of 1.
+//In string 3 we are assigning the new value "1" to the variable "count". "=" is the assignment's operator, which assigns the value on the right (count + 1) to the variable on the left (count). So after line 3 executes, the variable "count" will have the new value of 1.
