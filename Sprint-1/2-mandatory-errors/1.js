de6c77e9e60f4b294442634bec02a93e58f07036
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-13 18:10:21 +0100

    Change const into let

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+//in order to reassign a value to a variable, it must be declared with "let" instead of "const". in this case we should declare the variable "age" with let, to be able to reassign the value of age by 1, which will result in 34.
+console.log(age);
