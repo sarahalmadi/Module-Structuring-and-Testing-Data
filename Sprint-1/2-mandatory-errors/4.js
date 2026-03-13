4a18f6c96bb20c768ae1ad2433e7775eb65e852e
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-13 19:10:17 +0100

    Fix the clock format

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twentyFourHourClockTime = "20:53"; // the variable must be in 24 hour clock format, so we should change the value of the variable to be in 24 hour format.
+const twelveHourClockTime = "08:53"; // the variable must be in 12 hour clock format, so we should change the value of the variable to be in 12 hour format.
+console.log(twentyFourHourClockTime);
+console.log(twelveHourClockTime);
