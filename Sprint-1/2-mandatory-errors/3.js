345dae6439c5d08b3bdc6d36ac316b0444bd8297
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-13 18:55:31 +0100

    Fix the error

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,6 +1,7 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
-
+const last4Digits = cardNumber.toString().slice(-4);
+const masked = last4Digits.padStart(cardNumber.toString().length, "*");
+console.log(masked);
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
