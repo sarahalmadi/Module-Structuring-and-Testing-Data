57d4461fe7965549fbc5fb4bd80bcc082ceea25f
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-15 16:04:50 +0100

    Solve it

---
+++ random/toast.js
@@ -0,0 +1,11 @@
+function sixToast(num) {
+  if (num > 6) return num - 6;
+  else return 6 - num;
+
+  // you code goes below this line
+}
+
+console.log(sixToast(1)); // 5
+console.log(sixToast(5)); // 1
+console.log(sixToast(6)); // 0
+console.log(sixToast(7)); // 1
