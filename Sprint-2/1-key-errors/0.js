244dc95a6317bfeaec90c92ad9c42df865cee900
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-20 22:47:34 +0100

    Fix The Syntax Error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,15 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
-// call the function capitalise with a string input
+// call the function capitalize with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+function capitalize(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
 // =============> write your explanation here
 // =============> write your new code here
+//SyntaxError: missing ) after argument list
+//The error occurs with the word capitalize
