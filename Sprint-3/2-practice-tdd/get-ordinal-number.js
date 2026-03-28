ba2ddefd2c34c7b8f3f70cecc6c15180c62e6f7d
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-28 19:04:02 +0100

    Fix It To Handle Special Cases

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,27 @@
 function getOrdinalNumber(num) {
-  return "1st";
-}
+  // special cases for 11, 12, 13
+  if (num % 100 >= 11 && num % 100 <= 13) {
+    return num + "th";
+  }
 
+  const lastDigit = num % 10;
+  if (lastDigit === 1) {
+    return num + "st";
+  } else if (lastDigit === 2) {
+    return num + "nd";
+  } else if (lastDigit === 3) {
+    return num + "rd";
+  } else {
+    return num + "th";
+  }
+}
 module.exports = getOrdinalNumber;
+console.log(getOrdinalNumber(1)); // "1st"
+console.log(getOrdinalNumber(2)); // "2nd"
+console.log(getOrdinalNumber(3)); // "3rd"
+console.log(getOrdinalNumber(4)); // "4th"
+console.log(getOrdinalNumber(11)); // "11th"
+console.log(getOrdinalNumber(12)); // "12th"
+console.log(getOrdinalNumber(13)); // "13th"
+console.log(getOrdinalNumber(21)); // "21st"
+console.log(getOrdinalNumber(22)); // "22nd"
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,4 +1,5 @@
 const getOrdinalNumber = require("./get-ordinal-number");
+
 // In this week's prep, we started implementing getOrdinalNumber
 
 // continue testing and implementing getOrdinalNumber for additional cases
@@ -10,4 +11,31 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+});
+// Case 2: Identify the ordinal number for 2
+// When the number is 2,
+// Then the function should return "2nd"
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+// Case 3: Identify the ordinal number for 3
+// When the number is 3,
+// Then the function should return "3rd"
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+
+test("should return '11th' for 11", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+});
+
+test("should return '12th' for 12", () => {
+  expect(getOrdinalNumber(12)).toEqual("12th");
+});
+test("should return '13th' for 13", () => {
+  expect(getOrdinalNumber(13)).toEqual("13th");
+});
+
+test("should return '21st' for 21", () => {
+  expect(getOrdinalNumber(21)).toEqual("21st");
 });
